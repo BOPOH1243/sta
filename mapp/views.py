d4944deae4eaa7348c82306c30d8fa03be0715b2
BOPOH1243 <tenpenni@mail.ru>
2022-11-28 08:09:47 -0500

    сделал часть вьюсетов

--- mapp/views.py
+++ mapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework import permissions
+from .models import *
+from .serializers import *
 # Create your views here.
+
+
+class LevelViewset(viewsets.ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+
+
+class PerevalViewset(viewsets.ModelViewSet):
+    queryset = Pereval.objects.all()
+    serializer_class = PerevalSerializer
+
+#FIXME добавить все вьюсеты
